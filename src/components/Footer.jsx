77715e894ae45f9144cc7a03dfae5a1b41be19d9
Ferdous Ahmed Himel <himibaba10@gmail.com>
2024-05-15 20:20:32 +0600

    Added the footer section

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,10 @@
 const Footer = () => {
-  return <div>Footer will be set here</div>;
+  const currentYear = new Date().getFullYear();
+  return (
+    <footer className="bg-[#11173b] text-center py-5 text-white text-xs">
+      COPYRIGHT ©{currentYear} ALL RIGHTS RESERVED
+    </footer>
+  );
 };
 
 export default Footer;
